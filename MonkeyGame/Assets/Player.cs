67052423df392925e5653bcb6ebb9a939653b114
joeman8000 <jpacentine1@gmail.com>
2022-11-22 22:31:30 -0500

    player jump

--- MonkeyGame/Assets/Player.cs
+++ MonkeyGame/Assets/Player.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower;
+    private float lungPower;
+    public float y = .04f;
+    public float x = .01f;
+
+
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(IsGrounded() && rb.velocity.y < 0)
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+
+        if (Input.GetButton("Jump") && IsGrounded())
+        {
+            jumpingPower += y;
+            lungPower += x;
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            rb.velocity = new Vector2(jumpingPower, jumpingPower);
+            jumpingPower = 0;
+            lungPower = 0;
+        }
+
     }
+
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+
 }
